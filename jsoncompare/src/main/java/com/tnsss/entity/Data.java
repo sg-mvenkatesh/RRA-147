72495f1d4593f46a53e54e5efb541902405f22ee
Mohan Venkatesh <101633546+sg-mvenkatesh@users.noreply.github.com>
2022-03-31 19:24:55 +0530

    Updated on Compare Two json Task
    Updated on 31-march-2022

---
+++ jsoncompare/src/main/java/com/tnsss/entity/Data.java
@@ -0,0 +1,28 @@
+package com.tnsss.entity;
+
+
+public class Data {
+	
+	private ProductRateDetails[] productRateDetails;
+
+    private VehicleDetails vehicleDetails;
+
+    private DealerDetails dealerDetails;
+    
+    public ProductRateDetails[] getProductRateDetails() {
+		return productRateDetails;
+	}
+
+	public void setProductRateDetails(ProductRateDetails[] productRateDetails) {
+		this.productRateDetails = productRateDetails;
+	}
+
+
+
+
+	@Override
+    public String toString()
+    {
+        return "ClassPojo [productRateDetails = "+productRateDetails+"]";
+    }
+}
---
+++ jsoncompare/src/test/java/com/tnsss/json/CompareTwoJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsss.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompareTwoJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
